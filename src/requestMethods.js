074e03e65c2114a6799bf4b6a33d2193a0d7afb5
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-17 15:47:50 +0530

    token to be parsed only when userExists

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+const userExists = JSON.parse(
+  JSON.parse(localStorage.getItem("persist:root")).user
+).currentUser;
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
+const TOKEN = userExists
+  ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+      .currentUser.accessToken
+  : "";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
