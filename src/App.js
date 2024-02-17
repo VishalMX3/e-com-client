8e30dc9e4d348d0a85b5bb8a81be386eb2dc91fb
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-17 11:27:19 +0530

    Client UI completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
 function App() {
-  return <div className="App">Hey</div>;
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products/:category" element={<ProductList />} />
+        <Route exact path="/product/:id" element={<Product />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/success" element={<Success />} />
+        <Route
+          exact
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          exact
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import Product from "./Product";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `https://e-com-api-inip.onrender.com/api/products?category=${cat}`
+            : "https://e-com-api-inip.onrender.com/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+    console.log(filteredProducts);
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+    console.log(filteredProducts);
+  }, [sort]);
+
+  return (
+    <Container>
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item._id} />)
+        : products
+            .slice(0, 5)
+            .map((item) => <Product item={item} key={item._id} />)}
+    </Container>
+  );
+};
+
+export default Products;
