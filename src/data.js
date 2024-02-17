e800f01d1c3fbd36275ddb11e24216071e04e048
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-17 18:40:39 +0530

    updated categories

--- src/data.js
+++ src/data.js
@@ -60,20 +60,20 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "SHIRT STYLE!",
-    cat: "women",
+    img: "https://images.pexels.com/photos/3739943/pexels-photo-3739943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "MEN",
+    cat: "men",
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "LOUNGEWEAR LOVE",
-    cat: "coat",
+    img: "https://images.pexels.com/photos/14629993/pexels-photo-14629993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "KIDS",
+    cat: "kids",
   },
   {
     id: 3,
     img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    title: "LIGHT JACKETS",
-    cat: "jeans",
+    title: "WOMEN",
+    cat: "women",
   },
 ];
