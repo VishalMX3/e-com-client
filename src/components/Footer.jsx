34b703fe7d10bf047baca0036917705abcad81d1
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-18 21:32:18 +0530

    updated address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -126,13 +126,14 @@ const Footer = () => {
       <Right>
         <Title>Contact</Title>
         <ContactItem>
-          <Room style={{ marginRight: "10px" }} /> Mal C, BITS Pilani - 333031
+          <Room style={{ marginRight: "10px" }} /> BITS Pilani - 333031
         </ContactItem>
         <ContactItem>
           <Phone style={{ marginRight: "10px" }} /> +1 234 56 78
         </ContactItem>
         <ContactItem>
-          <MailOutline style={{ marginRight: "10px" }} /> contact@vishal.tech
+          <MailOutline style={{ marginRight: "10px" }} />{" "}
+          vishalpundhirofficial@gmail.com
         </ContactItem>
         <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
       </Right>
