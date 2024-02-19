636d7ce3d2b4a24598e92d8d2ece849df26c55b0
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-19 13:53:36 +0530

    Added persist root if not there

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// Check if persist:root key exists, if not, initialize it with an empty object
+if (!localStorage.getItem("persist:root")) {
+  localStorage.setItem(
+    "persist:root",
+    JSON.stringify({
+      user: '{"currentUser":null,"isFetching":false,"error":false}',
+      cart: '{"products":[],"quantity":0,"total":0}',
+      _persist: '{"version":1,"rehydrated":true}',
+    })
+  );
+}
+
 const userExists = JSON.parse(
   JSON.parse(localStorage.getItem("persist:root")).user
 ).currentUser;
